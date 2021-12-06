501e6296d1ce4c12b8b6c410f0dc97982574a631
BenHardin <bbhardin1@gmail.com>
2021-12-06 15:39:38 -0500

    Begins work on SEMPRE integration, adds description of how the user request was parsed

--- CS592 Project/CS592 Project/ContentView.swift
+++ CS592 Project/CS592 Project/ContentView.swift
@@ -8,31 +8,70 @@
 import SwiftUI
 import Quartz
 
-// Preview code taken from https://stackoverflow.com/questions/59696367/how-do-i-show-qlpreviewpanel-with-swiftui-on-macos
-func loadPreviewItem(with name: String) -> NSURL {
-
-    //let file = name.components(separatedBy: ".")
-    //let path = Bundle.main.path(forResource: file.first!, ofType: file.last!)
-    let url = NSURL(fileURLWithPath: name)
-
-    return url
-}
-
-struct MyPreview: NSViewRepresentable {
-    var fileName: String
-
-    func makeNSView(context: NSViewRepresentableContext<MyPreview>) -> QLPreviewView {
-        let preview = QLPreviewView(frame: .init(x: 0, y: 0, width: 100, height: 100), style: .normal)
-        preview?.autostarts = true
-        preview?.previewItem = loadPreviewItem(with: fileName) as QLPreviewItem
-        return preview ?? QLPreviewView()
+func parseSEMPREOutput(input: String) -> [String] {
+    // Example: (and (type presentation) (year 2019) (folder Downloads) (downloaded_from xyz.com))
+    
+    print("here")
+    
+    var argumentsArray : [String] = []
+    
+    var mdfindQuery = ""
+    let terms = input.components(separatedBy: "(")
+    for term in terms {
+        let kindAndName = term.components(separatedBy: " ")
+        let kind = kindAndName.first
+        var query = kindAndName.last ?? ""
+        query.removeAll(where: {$0 == ")"})
+        
+        switch(kind) {
+        case "type":
+            // Options accepted by mdfind for kind/type
+            let kindOptions = ["application", "applications", "app", "audio", "music", "bookmark", "bookmarks", "contact", "contacts", "email", "emails", "mail message", "mail messages", "folder", "folders", "font", "fonts", "event", "events", "todo", "todos", "to do", "to dos", "image", "images", "movie", "movies", "pdf", "pdfs", "system preferences", "preferences", "presentations", "presentation"]
+            
+            if (kindOptions.contains(query)) {
+                mdfindQuery += " kind:" + query
+                argumentsArray.append("kind")
+                argumentsArray.append(query)
+            }
+            break
+        case "year":
+            
+            if (query == Calendar.current.component(.year, from: Date()).description) {
+                query = "this year"
+            }
+            
+            // Options accepted by mdfind for date
+            let dateOptions = ["today", "yesterday", "this week", "this month", "this year", "tomorrow", "next month", "next week", "next year"]
+            
+            if (dateOptions.contains(query)) {
+                mdfindQuery += " date:" + query
+                argumentsArray.append("date")
+                argumentsArray.append(query)
+            }
+            print(query)
+            break
+        case "folder":
+            let folderOptions = ["Documents", "Downloads", "Desktop"]
+            if (folderOptions.contains(query)) {
+                mdfindQuery += " -onlyin " + query
+                argumentsArray.append("-onlyin")
+                argumentsArray.append(query)
+            }
+            
+            break
+        case "downloaded_from":
+            mdfindQuery += " \"kMDItemWhereFroms:" + query + "\""
+            argumentsArray.append("Where From")
+            argumentsArray.append(query)
+            break
+        default:
+            break
+        }
     }
-
-    func updateNSView(_ nsView: QLPreviewView, context: NSViewRepresentableContext<MyPreview>) {
-    }
-
-    typealias NSViewType = QLPreviewView
-
+    
+    print(mdfindQuery)
+    return argumentsArray
+    
 }
 
 struct ContentView: View {
@@ -42,6 +81,10 @@ struct ContentView: View {
     @State var searchResultsArray = [""]
     @State var showResults = false
     @State var selectedColor = [Color](repeatElement(Color.clear, count: 8))
+    @State var argumentsArray : [String] = []
+    
+    // Show mdfind arguments
+    @State var showArguments = false
     
     var body: some View {
         
@@ -59,6 +102,8 @@ struct ContentView: View {
                     if (searchQuery == "") {
                         return
                     }
+                    
+                    argumentsArray = parseSEMPREOutput(input: searchQuery)
                     
                     let stdOut = Pipe()
                     let process = Process()
@@ -93,10 +138,19 @@ struct ContentView: View {
             }
             .padding(.top)
             
+            Button("Show Options", action: {
+                showArguments = !showArguments
+            })
             
-//            Text(self.searchResults)
-//                .padding()
-                
+            if (showArguments) {
+                HStack(alignment: .top) {
+                    ForEach((0...max(0,argumentsArray.count-1)), id: \.self) { i in
+                        HStack {
+                            Text(argumentsArray[i])
+                        }
+                    }
+                }
+            }
             
             if (showResults) {
                 HStack(alignment: .top) {
@@ -203,3 +257,31 @@ private func saveBookmarkData(for workDir: URL) {
         print("Failed to save bookmark data for \(workDir)", error)
     }
 }
+
+// Preview code taken from https://stackoverflow.com/questions/59696367/how-do-i-show-qlpreviewpanel-with-swiftui-on-macos
+func loadPreviewItem(with name: String) -> NSURL {
+
+    //let file = name.components(separatedBy: ".")
+    //let path = Bundle.main.path(forResource: file.first!, ofType: file.last!)
+    let url = NSURL(fileURLWithPath: name)
+
+    return url
+}
+
+struct MyPreview: NSViewRepresentable {
+    var fileName: String
+
+    func makeNSView(context: NSViewRepresentableContext<MyPreview>) -> QLPreviewView {
+        let preview = QLPreviewView(frame: .init(x: 0, y: 0, width: 100, height: 100), style: .normal)
+        preview?.autostarts = true
+        preview?.previewItem = loadPreviewItem(with: fileName) as QLPreviewItem
+        return preview ?? QLPreviewView()
+    }
+
+    func updateNSView(_ nsView: QLPreviewView, context: NSViewRepresentableContext<MyPreview>) {
+    }
+
+    typealias NSViewType = QLPreviewView
+
+}
+
